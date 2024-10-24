e813f788db2df762bafa06743db216bd6a86a37d
Łukasz Ochnio <ochnio.lukasz@gmail.com>
2024-10-24 13:38:12 +0200

    bug fix

--- ss_script.js
+++ ss_script.js
@@ -1,4 +1,4 @@
-
+/*
 function pracownicy(){
     var prac=document.getElementById("ss_pracownicy").value; 
     
@@ -20,7 +20,7 @@ function toggleSection() {
 
 
 function srednikosztsystemu(){
-  const srednikoszt = document.getElementById("ss_systemcost").value;;
+  const srednikoszt = document.getElementById("ss_systemcost").value;
   return srednikoszt;
 }
 function wa(){
@@ -33,7 +33,7 @@ function proc(){
     return proc;
 }
 
-function skargi(){
+function skargikoszt(){
     var skargi=document.getElementById("ss_skargi").value; 
     return skargi;
 }
@@ -60,7 +60,7 @@ function kosztpracownikow(){
     return kosztodpadow;
   }
   function kosztskarg() {
-    const kosztsk = skargi()*koszt();
+    const kosztsk = skargikoszt()*koszt();
     return kosztsk;
   }
 
@@ -84,6 +84,91 @@ function kosztpracownikow(){
     
   }
 
+*/
+function pracownicy() {
+  var prac = parseInt(document.getElementById("ss_pracownicy").value) || 0;
+  return prac;
+}
+
+function toggleSection() {
+var section = document.getElementById("costSection");
+var button = document.getElementById("costButton");
+
+if (section.style.display === "none") {
+    section.style.display = "block";
+    button.textContent = "▼Cost details";
+} else {
+    section.style.display = "none";
+    button.textContent = "►Cost details";
+}
+}
+
+function srednikosztsystemu() {
+const srednikoszt = parseFloat(document.getElementById("ss_systemcost").value) || 0;
+return srednikoszt;
+}
+
+function wa() {
+  var wad = parseFloat(document.getElementById("ss_wady").value) || 0;
+  return wad;
+}
+
+function proc() {
+  var proc = parseFloat(document.getElementById("ss_procent").value) || 0;
+  return proc;
+}
+
+function skargikoszt() {
+  var skargi = parseFloat(document.getElementById("ss_skargi").value) || 0;
+  return skargi;
+}
+
+function koszt() {
+var koszt = parseFloat(document.getElementById("ss_koszt").value) || 0;
+return koszt;
+}
+
+function kosztwady() {
+var kosztw = parseFloat(document.getElementById("ss_kosztwady").value) || 0;
+return kosztw;
+}
+
+function kosztpracownika() {
+var koszt = parseFloat(document.getElementById("ss_operatorcost").value) || 0;
+return koszt;
+}
+
+function kosztpracownikow() {
+var kosztp = kosztpracownika() * pracownicy();
+return kosztp;
+}
+
+function iloscodpadow() {
+  const iloscodpad = wa() / 100 * proc();
+  const kosztodpadow = iloscodpad * kosztwady();
+  return kosztodpadow;
+}
+
+function kosztskarg() {
+  const kosztsk = skargikoszt() * koszt();
+  return kosztsk;
+}
+
+function sumakosztow() {
+  const sumkosz = kosztskarg() + iloscodpadow() + kosztpracownikow();
+  return sumkosz;
+}
+
+function zwrot() {
+  const zwrotwm = srednikosztsystemu() / sumakosztow();
+  var miesiac = Math.round(zwrotwm);
+  return miesiac;
+}
+
+function display() {
+  document.getElementById("ss_roi").innerHTML = sumakosztow();
+  document.getElementById("ss_mnth").innerHTML = zwrot();
+}
 
 
 
